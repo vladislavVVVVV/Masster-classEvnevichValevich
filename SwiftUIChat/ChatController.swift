e65671d36161a1a30bd9b8620516701c3676a274
Vlad Evnevich <uladzislau.yaunevich@innowise-group.com>
2021-02-24 16:30:58 +0300

    Initial commit

---
+++ SwiftUIChat/ChatController.swift
@@ -0,0 +1,40 @@
+//
+//  ChatController.swift
+//  SwiftUIChat
+//
+//  Created by Илья Валевич on 2/24/21.
+//  Copyright © 2021 Илья Валевич. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+// ChatController needs to be a ObservableObject in order
+// to be accessible by SwiftUI
+class ChatController : ObservableObject {
+    // didChange will let the SwiftUI know that some changes have happened in this object
+    // and we need to rebuild all the views related to that object
+    var didChange = PassthroughSubject<Void, Never>()
+    
+    // We've relocated the messages from the main SwiftUI View. Now, if you wish, you can handle the networking part here and populate this array with any data from your database. If you do so, please share your code and let's build the first global open-source chat app in SwiftUI together
+    @Published var messages = [
+        ChatMessage(message: "Hello world", avatar: "A", color: .red),
+        ChatMessage(message: "Hi", avatar: "B", color: .blue)
+    ]
+    
+    // this function will be accessible from SwiftUI main view
+    // here you can add the necessary code to send your messages not only to the SwiftUI view, but also to the database so that other users of the app would be able to see it
+    func sendMessage(_ chatMessage: ChatMessage) {
+        // here we populate the messages array
+        messages.append(chatMessage)
+        // here we let the SwiftUI know that we need to rebuild the views
+        didChange.send(())
+    }
+    
+}
+
+struct ChatController_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ SwiftUIChat/ContentView.swift
@@ -0,0 +1,103 @@
+//
+//  ContentView.swift
+//  SwiftUIChat
+//
+//  Created by Илья Валевич on 2/24/21.
+//  Copyright © 2021 Илья Валевич. All rights reserved.
+//
+
+import SwiftUI
+
+// let's create a structure that will represent each message in chat
+struct ChatMessage : Hashable {
+    var message: String
+    var avatar: String
+    var color: Color
+    var isMe: Bool = false
+}
+
+struct ContentView : View {
+    
+     // @State here is necessary to make the composedMessage variable accessible from different views
+    @State var composedMessage: String = ""
+    @EnvironmentObject var chatController: ChatController
+    
+    var body: some View {
+      
+        // the VStack is a vertical stack where we place all our substacks like the List and the TextField
+        VStack {
+            // I've removed the text line from here and replaced it with a list
+            // List is the way you should create any list in SwiftUI
+            List {
+                // we have several messages so we use the For Loop
+                ForEach(chatController.messages, id: \.self) { msg in
+                    ChatRow(chatMessage: msg)
+                }
+            }
+            
+            // TextField are aligned with the Send Button in the same line so we put them in HStack
+            HStack {
+                // this textField generates the value for the composedMessage @State var
+                TextField("Message...", text: $composedMessage).frame(minHeight: CGFloat(30))
+                // the button triggers the sendMessage() function written in the end of current View
+                Button(action: sendMessage) {
+                    Text("Send")
+                }
+            }.frame(minHeight: CGFloat(50)).padding()
+            // that's the height of the HStack
+        }
+    }
+    func sendMessage() {
+        chatController.sendMessage(ChatMessage(message: composedMessage, avatar: "C", color: .green, isMe: true))
+        composedMessage = ""
+    }
+}
+
+// ChatRow will be a view similar to a Cell in standard Swift
+struct ChatRow : View {
+    // we will need to access and represent the chatMessages here
+    var chatMessage: ChatMessage
+    // body - is the body of the view, just like the body of the first view we created when opened the project
+    var body: some View {
+        // HStack - is a horizontal stack. We let the SwiftUI know that we need to place
+        // all the following contents horizontally one after another
+        Group {
+            if !chatMessage.isMe {
+                HStack {
+                    Group {
+                        Text(chatMessage.avatar)
+                        Text(chatMessage.message)
+                            .bold()
+                            .padding(10)
+                            .foregroundColor(Color.white)
+                            .background(chatMessage.color)
+                            .cornerRadius(10)
+                    }
+                }
+            } else {
+                HStack {
+                    Group {
+                        Spacer()
+                        Text(chatMessage.message)
+                            .bold()
+                            .foregroundColor(Color.white)
+                            .padding(10)
+                            .background(chatMessage.color)
+                        .cornerRadius(10)
+                        Text(chatMessage.avatar)
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        .environmentObject(ChatController())
+    }
+}
+#endif
